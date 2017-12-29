41fff817933a6cdecad87f44800dd8cddb691343
ssydn1014 <ssydn1014@hmail.com>
2017-12-28 22:54:44 -0600

    second example

---
+++ Comments.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+print('single line comments are of five types. Which are as follows:')
+print('Single quotes')
+print("Double quotes")
+print('''triple single quotes''')
+print("""triple double quotes""")
+print("hash")
+print()
+print("multi line comments are of two types. Which are as follows:")
+print("""triple double quotes""")
+print('''triple single quotes''')
+
---
+++ NumbersExample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+accountID = 1233
+balance  = 33.45
+com = complex(3,4)
+print(float(accountID), type(accountID), id(accountID))
+print(int(balance), type(balance), id(balance),"converting the float in to int")
+print(com, com.real, type(com),id(com))
+print(com, com.imag,type(com), id(com))
+print(com, com.conjugate(),type(com), id(com))
+
